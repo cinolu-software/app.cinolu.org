77f9eae2c35e7285ef630fde3f63f4c558f40558
JerryLunda <jerrylundakasongo@gmail.com>
2024-06-19 16:43:57 +0200

    feat: implement slidebar

--- src/Data/Layout/Menu.tsx
+++ src/Data/Layout/Menu.tsx
@@ -12,6 +12,43 @@ export const MenuList: MenuItem [] | undefined = [
         type: "link",
         title: "Dashboard"
       }
+      
     ]
+  },
+  {
+    title: "Administration",
+    lanClass: "lan-1",
+    menucontent: "admin",
+    Items: [
+      {
+        title: "User",
+        id: 1,
+        icon: "user",
+        type: "sub",
+        lanClass: "lan-3",
+        children: [
+          { path: "/dashboard/default_dashboard", title: "entrepreneur", type: "link", lanClass: "lan-4" },
+          { path: "/dashboard/project", title: "coach", type: "link", lanClass: "lan-5" },
+          { path: "/dashboard/ecommerce", title: "admin", type: "link" },
+        ],
+      },
+      {
+        title: "Programmes",
+        id: 2,
+        icon: "project",
+        type: "sub",
+        lanClass: "lan-6",
+        active: false,
+        children: [
+          { path: "/widgets/general", title: "Camp d'innovation", type: "link" },
+          { path: "/widgets/chart", title: "Stages", type: "link" },
+          { path: "/widgets/chart", title: "Incubation", type: "link" },
+          { path: "/widgets/general", title: "Mentors programs", type: "link" },
+          { path: "/widgets/chart", title: "F-360", type: "link" },
+          { path: "/widgets/chart", title: "Expo & Conférence", type: "link" },
+          { path: "/widgets/chart", title: "statut", type: "link" },
+        ],
+      },
+    ],
   }
 ];
