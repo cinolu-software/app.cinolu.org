4038c6347e052a0557272799cbdb742ec9535100
MosesKal <mosesziongo@gmail.com>
2024-10-19 11:51:12 +0200

    feat: adding of partners on program creation

--- src/Components/Applications/programs/AddProgram/index.tsx
+++ src/Components/Applications/programs/AddProgram/index.tsx
@@ -1,22 +1,33 @@
 import React from 'react';
-import {Card, CardBody, CardHeader, Col, Container, Row} from "reactstrap";
+import { Card, CardBody, CardHeader, Col, Container, Row } from "reactstrap";
 import Body from "@/Components/Applications/programs/AddProgram/Body";
+import Link from "next/link";
 
 const AddProgramContainer = () => {
-    return <Container fluid>
-        <Row>
-            <Col xs="12">
-                <Card>
-                    <CardHeader>
-                        <h5>{'Ajout de Programme'}</h5>
-                    </CardHeader>
-                    <CardBody>
-                        <Body />
-                    </CardBody>
-                </Card>
-            </Col>
-        </Row>
-    </Container>
+    return (
+        <Container fluid>
+            <Row className={'mb-4'}>
+                <Col className={'d-flex justify-content-end'}>
+                    <Link href={'/programs'} className={'btn btn-outline-primary'}>
+                        <i className="bi bi-arrow-left"></i>
+                        Retour
+                    </Link>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs="12">
+                    <Card>
+                        <CardHeader>
+                            <h5>{'Ajout de Programme'}</h5>
+                        </CardHeader>
+                        <CardBody>
+                            <Body />
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default AddProgramContainer;
