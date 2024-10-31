00b8c8e171b0f9ad297a23aa866857a390a62a94
MosesKal <mosesziongo@gmail.com>
2024-10-31 21:49:51 +0200

    feat: users privilege and role

--- src/Components/Applications/EditProfile/index.tsx
+++ src/Components/Applications/EditProfile/index.tsx
@@ -3,12 +3,21 @@ import { Container, Row , Col} from "reactstrap";
 import EditProfileForm from "./EditProfileImageAndPassword/EditProfileForm";
 import { toast, ToastContainer, Flip } from "react-toastify";
 import UserProfileInfo from "./UserProfileInfo/UserProfileInfo";
+import Link from "next/link";
 
 const EditProfileContainer = () => {
 
   return (
     <Container fluid>
       <div className="edit-profile">
+        <Row className={'mb-4'}>
+          <Col className={'d-flex justify-content-end'}>
+              <Link href={'/dashboard'} className={'btn btn-outline-primary'}>
+                  <i className="bi bi-arrow-left"></i>
+                  Retour
+              </Link>
+          </Col>
+        </Row>
         <Row>
           <UserProfileInfo/>
           <EditProfileForm />
