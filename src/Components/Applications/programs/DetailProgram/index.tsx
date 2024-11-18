a62db006fc3258673be84a3d4cc3bb39efa10876
MosesKal <mosesziongo@gmail.com>
2024-11-18 11:11:25 +0200

    feat: add partnair

--- src/Components/Applications/programs/DetailProgram/index.tsx
+++ src/Components/Applications/programs/DetailProgram/index.tsx
@@ -2,19 +2,24 @@ import React from 'react';
 import { Card, CardBody, CardHeader, Col, Container, Row } from "reactstrap";
 import Link from "next/link";
 import BackButton from "@/CommonComponent/BackButton";
+import {useAppDispatch, useAppSelector} from "@/Redux/Hooks";
+import Image from 'next/image';
+import {imageBaseUrl} from "@/services/axios";
 
 const DetailProgramContainer = () => {
+
+    const {selectedProgram} = useAppSelector(state=> state.programs);
+
+    console.log("====>", selectedProgram);
+
     return (
         <Container fluid>
             <BackButton link={'/programs'} />
             <Row>
                 <Col xs="12">
                     <Card>
-                        <CardHeader>
-                            <h5>{'Détail du Programme'}</h5>
-                        </CardHeader>
                         <CardBody>
-
+                            <img src={selectedProgram?.image ? `${imageBaseUrl}/programs/${selectedProgram?.image}` : ''} alt={''}  className={'img-fluid'} />
                         </CardBody>
                     </Card>
                 </Col>
