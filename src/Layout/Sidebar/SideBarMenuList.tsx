225088cb01ac5cef476fa8efa7cb512b7c48156e
MosesKal <mosesziongo@gmail.com>
2024-11-28 09:33:21 +0200

    fix: update profil

--- src/Layout/Sidebar/SideBarMenuList.tsx
+++ src/Layout/Sidebar/SideBarMenuList.tsx
@@ -1,19 +1,21 @@
-import { useAppSelector } from "@/Redux/Hooks";
-import { Fragment, useState } from "react";
-import { MenuList } from "@/Data/Layout/Menu";
-import { MenuItem } from "@/Types/LayoutTypes";
+import {Fragment, useEffect, useState} from "react";
+import {useAppSelector} from "@/Redux/Hooks";
+import {MenuList} from "@/Data/Layout/Menu";
+import {MenuItem} from "@/Types/LayoutTypes";
 import Menulist from "./Menulist";
-import { useTranslation } from "react-i18next";
+import {useTranslation} from "react-i18next";
 
 const SidebarMenuList = () => {
 
   const [activeMenu, setActiveMenu] = useState([]);
   const { pinedMenu } = useAppSelector((state) => state.layout);
+  const {user} = useAppSelector(state => state.auth)
   const { t } = useTranslation("common");
+  const [userRoles, setUserRoles] = useState<string[]>([]);
 
-
-  const userProfile = JSON.parse(localStorage.getItem("user_profile") || "{}");
-  const userRoles = userProfile.roles?.map((role:string) => role) || [];
+    useEffect(() => {
+      setUserRoles(user?.roles?.map((role:string) => role) || []);
+    }, [user]);
 
 
   const hasAccess = (requiredRoles?: string[]) => {
@@ -26,6 +28,8 @@ const SidebarMenuList = () => {
   const shouldHideMenu = (mainMenu: MenuItem) => {
     return mainMenu?.Items?.map((data) => data.title).every((titles) => pinedMenu.includes(titles || ""));
   };
+
+  console.log(user)
 
   return (
       <>
