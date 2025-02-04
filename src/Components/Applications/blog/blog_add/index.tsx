811b4e54e8ca36284e00b0b6107981bff0c615b7
MosesKal <mosesziongo@gmail.com>
2025-02-04 16:44:46 +0200

    feat: blog views

--- src/Components/Applications/blog/blog_add/index.tsx
+++ src/Components/Applications/blog/blog_add/index.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import CommonCardHeader from "@/CommonComponent/CommonCardHeader";
+import { BlogDiscardButton, BlogPostButton, PostEdit } from "@/Constant";
+import React from "react";
+import { Button, Card, CardBody, Col, Container, Row } from "reactstrap";
+import DropzoneClass from "@/Components/Applications/blog/blog_add/DropzoneClass";
+import FormPost from "@/Components/Applications/blog/blog_add/FormPost";
 
-const BlogAdd = () => {
+
+const AddPostContainer = () => {
+
     return (
-        <div></div>
-    )
-}
+        <Container fluid>
+            <Row>
+                <Col sm="12">
+                    <Card>
+                        <CommonCardHeader title={"Ajouter un article"} />
+                        <CardBody className="add-post">
+                            <FormPost />
+                            <DropzoneClass />
+                            <div className="btn-showcase text-end">
+                                <Button color="primary">{BlogPostButton}</Button>
+                                <Button color="light" type="reset">{BlogDiscardButton}</Button>
+                            </div>
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    );
 
-export default BlogAdd
+};
+
+export default AddPostContainer;
--- src/app/(MainBody)/blog/blog_add/page.tsx
+++ src/app/(MainBody)/blog/blog_add/page.tsx
@@ -1,22 +1,10 @@
 "use client";
-
 import React, {FunctionComponent, useState, useEffect} from "react";
+import AddPostContainer from "@/Components/Applications/blog/blog_add";
 
 const AddPost = () => {
 
-    const [MyAwesomeMap, setClient] = useState<FunctionComponent>();
-
-    useEffect(() => {
-        (async () => {
-            if(typeof window !== "undefined") {
-                const newClient = (await import("@/Components/Applications/blog/blog_add")).default;
-                setClient(() => newClient);
-            }
-        })
-    })
-
-    return MyAwesomeMap ? <MyAwesomeMap/> : "";
+    return <AddPostContainer/>
 }
 
 export default AddPost;
-
