b11b711bda56ff482320e362b936f92eef9a424d
MosesKal <mosesziongo@gmail.com>
2025-02-08 11:47:07 +0200

    feat: add article

--- src/app/(MainBody)/blog/blog_add/page.tsx
+++ src/app/(MainBody)/blog/blog_add/page.tsx
@@ -1,11 +1,22 @@
-"use client";
+"use client"
 
-import React from "react";
+import React, {FunctionComponent, useEffect, useState} from "react";
 import AddPostContainer from "@/Components/Applications/blog/blog_add";
 
 const AddPost = () => {
 
-    return <AddPostContainer/>
+    const [MyAwesomeMap, setClient] = useState<FunctionComponent>();
+
+    useEffect(() => {
+        (async () => {
+            if(typeof window !== "undefined"){
+                const newClient = (await import ('@/Components/Applications/blog/blog_add')).default;
+                setClient(()=>newClient);
+            }
+        })();
+    }, []);
+
+    return MyAwesomeMap ? <MyAwesomeMap/> : "";
 }
 
 export default AddPost;
