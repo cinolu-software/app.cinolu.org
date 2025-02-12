63fc057839a6962ba45102f8b78988a109c3b2b0
MosesKal <mosesziongo@gmail.com>
2025-02-12 11:57:51 +0200

    feat: base structure of chat room

--- src/Components/Applications/chat/chat_room/index.tsx
+++ src/Components/Applications/chat/chat_room/index.tsx
@@ -1,14 +1,27 @@
 import React,{useEffect, useState} from 'react';
 import {Container, Row} from 'reactstrap';
 import {useAppDispatch} from "@/Redux/Hooks";
+import {fetchChatApiData, fetchChatMemberApiData} from "@/Redux/Reducers/ChatSlice/ChatRoomSlice";
+import UserChat from "@/Components/Applications/chat/chat_room/UserChat";
+import LeftSideBar from "@/Components/Applications/chat/chat_room/LeftSideBar";
 
 
 const ChatRoomComponent = () => {
-    return (
-        <Container>
+    const dispatch = useAppDispatch()
 
+    useEffect(() => {
+        dispatch(fetchChatMemberApiData());
+        dispatch(fetchChatApiData());
+    }, []);
+
+    return (
+        <Container fluid>
+            <Row className="g-0">
+                <LeftSideBar />
+                <UserChat />
+            </Row>
         </Container>
-    )
+    );
 }
 
 export default ChatRoomComponent;
