f0fa089ccd797bc60e642a6ccaf6bf4f564f97d1
MosesKal <mosesziongo@gmail.com>
2025-03-18 10:14:42 +0200

    feat: chat room

--- src/Components/Applications/chat/chat_room/LeftSideBar/SearchNotFoundClass.tsx
+++ src/Components/Applications/chat/chat_room/LeftSideBar/SearchNotFoundClass.tsx
@@ -8,8 +8,8 @@ const SearchNotFoundClass :React.FC<SearchNotFoundClassType> = ({word}) => {
         <Col sm="12">
             <div>
                 <div className="search-not-found text-center p-5">
-                    <img className="img-100 mb-4" src={`${ImagePath}/other-images/sad4.gif`} alt="" />
-                    <p>{`Sorry, Not Found Any ${word}`}</p>
+                    {/* <img className="img-100 mb-4" src={`${ImagePath}/other-images/sad4.gif`} alt="" /> */}
+                    <p>{`Aucun utilisateur n'est actuellement connecté au chat.`}</p>
                 </div>
             </div>
         </Col>
