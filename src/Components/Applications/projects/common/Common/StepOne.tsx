d761a48ff51473e713d3c2e61487831b729d8771
MosesKal <mosesziongo@gmail.com>
2025-05-04 12:47:08 +0200

    feat: theme customize and processing

--- src/Components/Applications/projects/common/Common/StepOne.tsx
+++ src/Components/Applications/projects/common/Common/StepOne.tsx
@@ -4,13 +4,6 @@ import {StepPropsType} from "@/Types/Projects/ProjectType";
 import { setNewFormValue } from "@/Redux/Reducers/projectSlice/projectSlice";
 import { useAppDispatch } from "@/Redux/Hooks";
 import { activityAudience, activityDescription, activityName, activityPrise, activityTown, activityObjectif} from "@/Constant";
-import {Dropzone, ExtFile, FileMosaic} from "@dropzone-ui/react";
-import ReactQuill from "react-quill";
-import 'react-quill/dist/quill.snow.css';
-
-
-
-
 
 const StepOne: React.FC<StepPropsType> = ({ data }) => {
 
@@ -25,12 +18,73 @@ const StepOne: React.FC<StepPropsType> = ({ data }) => {
     return (
         <Form className="theme-form theme-form-2 mega-form">
             <Row className="g-2 mx-5">
-                <Label>Description de l'activité</Label>
-                <ReactQuill
-                    value={description}
-                    theme={'snow'}
-                    placeholder="Écrivez votre article ici..."
-                />
+                <Col xs="12">
+                    <Label className="col-form-label">{activityName}</Label>
+                    <Input
+                        className={name !== "" ? "valid" : "is-invalid"}
+                        type="text"
+                        required
+                        name="name"
+                        value={name}
+                        onChange={handleChange("name")}
+                    />
+                </Col>
+                <Col xs="12">
+                    <Label className="col-form-label">{activityTown}</Label>
+                    <Input
+                        className={town !== "" ? "valid" : "is-invalid"}
+                        type="text"
+                        required
+                        name="town"
+                        value={town}
+                        onChange={handleChange("town")}
+                    />
+                </Col>
+
+                <Col xs="12">
+                    <Label className="col-form-label">{activityAudience}</Label>
+                    <textarea
+                        rows={5}
+                        className="form-control"
+                        name="targeted_audience"
+                        value={targeted_audience}
+                        onChange={handleChange("targeted_audience")}
+                    />
+                </Col>
+
+
+                <Col xs="12">
+                    <Label className="col-form-label">{activityPrise}</Label>
+                    <textarea
+                        rows={5}
+                        className="form-control"
+                        name="prize"
+                        value={prize}
+                        onChange={handleChange("prize")}
+                    />
+                </Col>
+
+                <Col xs="12">
+                    <Label className="col-form-label">{activityObjectif}</Label>
+                    <textarea
+                        rows={10}
+                        className="form-control"
+                        name="aim"
+                        value={aim}
+                        onChange={handleChange("aim")}
+                    />
+                </Col>
+
+                <Col xs="12">
+                    <Label className="col-form-label">{activityDescription}</Label>
+                    <textarea
+                        rows={10}
+                        className="form-control"
+                        name="description"
+                        value={description}
+                        onChange={handleChange("description")}
+                    />
+                </Col>
             </Row>
         </Form>
     );
